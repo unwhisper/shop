009c7cbc6c895dbec0422a944ebb5a1f387e6659
leijie <591856607@qq.com>
2018-09-07 18:00:58 +0800

    'add'

--- application/common.php
+++ application/common.php
@@ -157,15 +157,120 @@ function is_weixin()
     }
 }
 
-//curl post请求
-function http_post($url, $data) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,$url);
-    curl_setopt($ch, CURLOPT_HEADER,0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    $res = curl_exec($ch);
-    curl_close($ch);
-    return $res;
+/**
+ * CURL请求
+ * @param $url string 请求url地址
+ * @param $method string 请求方法 get post
+ * @param mixed $postfields post数据数组
+ * @param array $headers 请求header信息
+ * @param bool|false $debug  调试开启 默认false
+ * @return mixed
+ */
+function httpRequest($url, $method="GET", $postfields = null, $headers = array(), $debug = false)
+{
+    $method = strtoupper($method);
+    $ci = curl_init();
+    /* Curl settings */
+    curl_setopt($ci, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+    curl_setopt($ci, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0");
+    curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 60); /* 在发起连接前等待的时间，如果设置为0，则无限等待 */
+    curl_setopt($ci, CURLOPT_TIMEOUT, 7); /* 设置cURL允许执行的最长秒数 */
+    curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
+    switch ($method) {
+        case "POST":
+            curl_setopt($ci, CURLOPT_POST, true);
+            if (!empty($postfields)) {
+                $tmpdatastr = is_array($postfields) ? http_build_query($postfields) : $postfields;
+                curl_setopt($ci, CURLOPT_POSTFIELDS, $tmpdatastr);
+            }
+            break;
+        default:
+            curl_setopt($ci, CURLOPT_CUSTOMREQUEST, $method); /* //设置请求方式 */
+            break;
+    }
+    $ssl = preg_match('/^https:\/\//i', $url) ? TRUE : FALSE;
+    curl_setopt($ci, CURLOPT_URL, $url);
+    if ($ssl) {
+        curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, FALSE); // https请求 不验证证书和hosts
+        curl_setopt($ci, CURLOPT_SSL_VERIFYHOST, FALSE); // 不从证书中检查SSL加密算法是否存在
+    }
+    //curl_setopt($ci, CURLOPT_HEADER, true); /*启用时会将头文件的信息作为数据流输出*/
+    if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) {
+        curl_setopt($ci, CURLOPT_FOLLOWLOCATION, 1);
+    }
+    curl_setopt($ci, CURLOPT_MAXREDIRS, 2);/*指定最多的HTTP重定向的数量，这个选项是和CURLOPT_FOLLOWLOCATION一起使用的*/
+    curl_setopt($ci, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ci, CURLINFO_HEADER_OUT, true);
+    /*curl_setopt($ci, CURLOPT_COOKIE, $Cookiestr); * *COOKIE带过去** */
+    $response = curl_exec($ci);
+    $requestinfo = curl_getinfo($ci);
+    $http_code = curl_getinfo($ci, CURLINFO_HTTP_CODE);
+    if ($debug) {
+        echo "=====post data======\r\n";
+        var_dump($postfields);
+        echo "=====info===== \r\n";
+        print_r($requestinfo);
+        echo "=====response=====\r\n";
+        print_r($response);
+    }
+    curl_close($ci);
+    return $response;
+    //return array($http_code, $response,$requestinfo);
+}
+
+/**
+ * 当前请求是否是https
+ * @return type
+ */
+function is_https()
+{
+    return isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && $_SERVER['HTTPS'] != 'off';
+}
+
+// 递归删除文件夹
+function delFile($path,$delDir = FALSE)
+{
+    if(!is_dir($path))
+        return FALSE;
+    $handle = @opendir($path);
+    if ($handle) {
+        while (false !== ( $item = readdir($handle) )) {
+            if ($item != "." && $item != "..")
+                is_dir("$path/$item") ? delFile("$path/$item", $delDir) : unlink("$path/$item");
+        }
+        closedir($handle);
+        if ($delDir) return rmdir($path);
+    }else {
+        if (file_exists($path)) {
+            return unlink($path);
+        } else {
+            return FALSE;
+        }
+    }
+}
+
+/**
+ * 获取随机字符串
+ * @param int $randLength  长度
+ * @param int $addtime  是否加入当前时间戳
+ * @param int $includenumber   是否包含数字
+ * @return string
+ */
+function get_rand_str($randLength=6,$addtime=1,$includenumber=0)
+{
+    if ($includenumber){
+        $chars='abcdefghijklmnopqrstuvwxyzABCDEFGHJKLMNPQEST123456789';
+    }else {
+        $chars='abcdefghijklmnopqrstuvwxyz';
+    }
+    $len=strlen($chars);
+    $randStr='';
+    for ($i=0;$i<$randLength;$i++){
+        $randStr.=$chars[rand(0,$len-1)];
+    }
+    $tokenvalue=$randStr;
+    if ($addtime){
+        $tokenvalue=$randStr.time();
+    }
+    return $tokenvalue;
 }
